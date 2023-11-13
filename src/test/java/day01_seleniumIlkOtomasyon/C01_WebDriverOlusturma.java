29e4770e3c8ba38d15aaa5c2f751d8a13eab7848
Nazime <kirkgoznazime@gmail.com>
2023-11-13 21:54:59 +0100

    20231113

---
+++ src/test/java/day01_seleniumIlkOtomasyon/C01_WebDriverOlusturma.java
@@ -0,0 +1,22 @@
+package day01_seleniumIlkOtomasyon;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class C01_WebDriverOlusturma {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("Webdriver.chrome.driver","src/resources/chromedriver.exe");
+        /*
+        bu satir olusturacagimiz webdririn öz.set ediyor
+        wedriver otomas.sirasinda  bizim elimiz kolumuz
+
+         */
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://www.testotomasyonu.com");
+        Thread.sleep(5000);
+        driver.manage().window().maximize();
+        Thread.sleep(5000);
+        driver.close();
+        driver.quit();
+    }
+}
